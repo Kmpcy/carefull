a75633ae86c8a55001c0d526b27bd1440abc6118
Karim <karimsalah795@gmail.com>
2025-10-01 20:58:08 +0300

    added api error model & generate added package build runner

---
+++ lib/core/networking/api_error_model.dart
@@ -0,0 +1,11 @@
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'api_error_model.g.dart';
+@JsonSerializable()
+class ApiEorrModel{
+final String meesage;
+final int statusCode;
+ApiEorrModel({required this.meesage,required this.statusCode});
+factory ApiEorrModel.fromJson(Map<String,dynamic>json)=>_$ApiEorrModelFromJson(json);
+Map<String,dynamic>toJson()=>_$ApiEorrModelToJson(this);
+}
